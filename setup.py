d7c51b9342b62343fc74cb3aa171118cf9b272b4
ajmm10 <angelon010@gmail.com>
2025-09-18 17:48:01 -0600

    Add estructura inicial desde cookiecutter-data-science (v1)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Actividades posteriores de la materia funadamentos de ingenieria de datos',
+    author='Angel-May',
+    license='MIT',
+)
